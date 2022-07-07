88963b999051893174f71f8723964bf0cbd63358
maralpbilisim <72076365+maralpbilisim@users.noreply.github.com>
2022-07-07 21:25:23 +0300

    son hali

--- javascript/odev1/odev1.js
+++ javascript/odev1/odev1.js
@@ -1,22 +1,31 @@
 let isim=prompt("isminizi giriniz")
 
-document.getElementById("myName").innerHTML=isim
+document.getElementById("myName").innerHTML=`${isim[0].toUpperCase()}${isim.slice(1).toLowerCase()}` //baş harfi büyütmek için bu kodu yazdım.
+    
 
-
-function showTime() {
-    const today = new Date();
-    let h = today.getHours();
-    let m = today.getMinutes();
-    let s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById("myClock").innerHTML =  h + ":" + m + ":" + s;
-    setTimeout(startTime, 1000);
-  }
-  
-  function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-  }
-  showTime()
-
+    
+    function showTime() {
+      let days = [
+        "Pazar",
+        "Pazartesi",
+        "Salı",
+        "Çarşamba",
+        "Perşembe",
+        "Cuma",
+        "Cumartesi",
+    ];
+        const today = new Date();
+        let h = today.getHours();
+        let m = today.getMinutes();
+        let s = today.getSeconds();
+        m = checkTime(m);
+        s = checkTime(s);
+        document.getElementById("myClock").innerHTML =  h + ":" + m + ":" + s +days[today.getDay()];
+        setTimeout(showTime, 1000);
+      }
+      
+      function checkTime(i) {
+        if (i < 10) {i = "0" + i};  
+        return i;
+      }
+     
