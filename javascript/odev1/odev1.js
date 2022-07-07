f5028305947aaf802aad2e4497da9ac66e61b94d
maralpbilisim <72076365+maralpbilisim@users.noreply.github.com>
2022-07-07 21:06:30 +0300

    son hali

---
+++ javascript/odev1/odev1.js
@@ -0,0 +1,22 @@
+let isim=prompt("isminizi giriniz")
+
+document.getElementById("myName").innerHTML=isim
+
+
+function showTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById("myClock").innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+  showTime()
+
